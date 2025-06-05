8e68c9e0cc24352406fee652fd99c4e591a319a8
syamchowdarydev <syamchowdarybalusu@gmail.com>
2025-06-05 23:17:50 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int items = 50;
+  float cost_per_item = 10.99;
+  float totalitems_cost = cost_per_item*totalitems_cost;
+  char dollar= $;
+
+cout << "No.of items: " << items << "\n" <<
+  cout << "Cost per item: " << cost_per_item <<"\n" <<
+  cout << "Cost of total items: " << toataitems_cost << dollar 
