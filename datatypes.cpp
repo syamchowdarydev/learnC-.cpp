dd4676c5768f5bd52ac497267e715bddd4608ea0
syamchowdarydev <syamchowdarybalusu@gmail.com>
2025-06-05 23:20:48 +0530

    Update datatypes.cpp

--- datatypes.cpp
+++ datatypes.cpp
@@ -5,9 +5,9 @@ int main() {
   int items = 50;
   float cost_per_item = 10.99;
   float totalitems_cost = cost_per_item*totalitems_cost;
-  char dollar= $;
+  char dollar= '$';
 
 cout << "No.of items: " << items << "\n";
   cout << "Cost per item: " << cost_per_item <<"\n";
-  cout << "Cost of total items: " << toataitems_cost << dollar;
+  cout << "Cost of total items: " << totalitems_cost << dollar;
 }
